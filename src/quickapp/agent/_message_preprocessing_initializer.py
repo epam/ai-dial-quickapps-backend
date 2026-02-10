c5c49c8a30abc3479399c03d2a1c3b996f6ce7bd
Daniil Yarmalkevich <daniil_yarmalkevich@epam.com>
2026-02-10 09:41:50 +0200

    addressed PR comments

--- src/quickapp/agent/_message_preprocessing_initializer.py
+++ src/quickapp/agent/_message_preprocessing_initializer.py
@@ -2,7 +2,7 @@ import logging
 
 from injector import inject
 
-from quickapp.common.base_initializer import BaseInitializer
+from quickapp.common.base_initializer import CompletionInitializer
 from quickapp.common.base_transformer import MessagesTransformer
 from quickapp.common.messages_mixin import MessagesMixin
 
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 @inject
-class _MessagePreprocessingInitializer(BaseInitializer):
+class _MessagePreprocessingInitializer(CompletionInitializer):
     def __init__(
         self,
         messages_context: MessagesMixin,
@@ -24,6 +24,4 @@ class _MessagePreprocessingInitializer(BaseInitializer):
         for transformer in self.__transformers:
             messages = transformer.transform(messages)
             logger.debug(f"{type(transformer).__name__}: {{\"result\": {messages}}}")
-        actual_messages = self.__messages_context.messages
-        actual_messages.clear()
-        actual_messages.extend(messages)
+        self.__messages_context.messages[:] = messages
