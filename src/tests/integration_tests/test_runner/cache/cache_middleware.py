0d99ec059d19598dd0660b1d32af493572047bb7
OleksandrGubarets <oleksandr_gubarets@epam.com>
2026-04-28 13:22:11 +0300

    fix wrong cache path

--- src/tests/integration_tests/test_runner/cache/cache_middleware.py
+++ src/tests/integration_tests/test_runner/cache/cache_middleware.py
@@ -38,7 +38,8 @@ AGENT_MODELS = [
     "gemini-2.5-pro",
     "gemini-3-pro-preview",
     "us.anthropic.claude-3-7-sonnet-20250219-v1",
-    "anthropic.claude-sonnet-4-5-20250929-v1:0" "anthropic.claude-v4-5-sonnet-v1",
+    "anthropic.claude-sonnet-4-5-20250929-v1:0",
+    "anthropic.claude-v4-5-sonnet-v1",
 ]
 
 
